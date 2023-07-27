bd13cf3fc7bd8924a042de1ae2e06e465305ed29
ceg0tr <gontanal2008@gmail.com>
2023-07-27 11:14:31 +0300

    heartcheck-1

--- my_app.py
+++ my_app.py
@@ -1,1 +1,41 @@
-# напиши здесь код основного приложения и первого экрана
+from random import randint
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLabel, QVBoxLayout, QHBoxLayout
+from instr import *
+
+app = QApplication([])
+
+class MainWin(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.set_appear()
+        self.initUI()
+        self.connects()
+        self.show()
+    def set_appear(self):
+        self.setWindowTitle(txt_title)
+        self.resize(win_width, win_height)
+        self.move(win_x, win_y)
+    def initUI(self):
+        self.hello_text = QLabel(txt_hello)
+        self.instruction = QLabel(txt_instruction)
+        self.button = QPushButton(txt_next)
+        self.layout = QVBoxLayout()
+        
+        self.layout.addWidget(self.hello_text, alignment = Qt.AlignCenter)
+        self.layout.addWidget(self.instruction, alignment = Qt.AlignCenter)
+        self.layout.addWidget(self.button, alignment = Qt.AlignCenter)
+        self.setLayout(self.layout)
+    def connects(self):
+        self.button.clicked.connect(self.next_click)
+    def next_click(self):
+        self.hide()
+        self.tw = TestWin()
+
+
+window1 = MainWin()
+
+
+
+app.exec_()
+window1.show()
