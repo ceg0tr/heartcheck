c0f8afd223292cd4fee83c0a62e282ba3e465e6b
ceg0tr <gontanal2008@gmail.com>
2023-08-03 11:02:08 +0300

    a

--- my_app.py
+++ my_app.py
@@ -1,41 +1,41 @@
-from random import randint
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLabel, QVBoxLayout, QHBoxLayout
+from PyQt5.QtWidgets import (QApplication, QWidget, QVBoxLayout, QPushButton, QLabel)
 from instr import *
-
-app = QApplication([])
-
+from second_win import *
+ 
 class MainWin(QWidget):
     def __init__(self):
         super().__init__()
-        self.set_appear()
         self.initUI()
         self.connects()
+        self.set_appear()
         self.show()
+ 
+    def initUI(self):
+        self.btn_next = QPushButton(txt_next, self)
+        self.hello_text = QLabel(txt_hello)
+        self.instruction = QLabel(txt_instruction)
+ 
+ 
+        self.layout_line = QVBoxLayout()
+        self.layout_line.addWidget(self.hello_text, alignment = Qt.AlignLeft)
+        self.layout_line.addWidget(self.instruction, alignment = Qt.AlignLeft)
+        self.layout_line.addWidget(self.btn_next, alignment = Qt.AlignCenter)         
+        self.setLayout(self.layout_line)
+ 
+    def next_click(self):
+        self.tw = TestWin()
+        self.hide()
+ 
+    def connects(self):
+        self.btn_next.clicked.connect(self.next_click)
+ 
     def set_appear(self):
         self.setWindowTitle(txt_title)
         self.resize(win_width, win_height)
         self.move(win_x, win_y)
-    def initUI(self):
-        self.hello_text = QLabel(txt_hello)
-        self.instruction = QLabel(txt_instruction)
-        self.button = QPushButton(txt_next)
-        self.layout = QVBoxLayout()
-        
-        self.layout.addWidget(self.hello_text, alignment = Qt.AlignCenter)
-        self.layout.addWidget(self.instruction, alignment = Qt.AlignCenter)
-        self.layout.addWidget(self.button, alignment = Qt.AlignCenter)
-        self.setLayout(self.layout)
-    def connects(self):
-        self.button.clicked.connect(self.next_click)
-    def next_click(self):
-        self.hide()
-        self.tw = TestWin()
-
-
+ 
+app = QApplication([])
 mw = MainWin()
-
-
-
 app.exec_()
-window1.show()
+ 
